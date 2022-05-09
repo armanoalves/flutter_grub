ca53200d05e23edd7f8468cba260bf8c6f120a9c
Armano <armanobarrosalvesh@gmail.com>
2022-05-08 22:16:00 -0300

    Arquivos de estudo

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2022/05/04/15/42/dog-7174266_960_720.jpg'),
+  '2': const User(
+      id: '2',
+      name: 'Alan',
+      email: 'alan@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg'),
+  '3': const User(
+      id: '3',
+      name: 'Heitor',
+      email: 'heitor@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg'),
+  '4': const User(
+      id: '4',
+      name: 'Henrique',
+      email: 'henri@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/02/07/16/47/kingfisher-2046453_960_720.jpg'),
+};
---
+++ lib/views/user_list.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/components/user_tile.dart';
+import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:provider/provider.dart';
+
+class UserList extends StatelessWidget {
+  const UserList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final UsersProvider users = Provider.of(context);
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Lista de usuários'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.USER_FORM);
+              },
+              icon: const Icon(Icons.add),
+            ),
+          ],
+        ),
+        body: ListView.builder(
+          itemCount: users.count,
+          itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
+        ));
+  }
+}
